1b413b5712805e027d538e62561b4a30d22ed405
naughtydev <pankajthakkar17@gmail.com>
2021-12-19 19:31:00 +0530

    Project initiated

---
+++ js/custom.js
@@ -0,0 +1,104 @@
+var images = [];
+images['grey-green'] = './images/grey-green/greygreen-1.jpg,./images/grey-green/greygreen-2.jpg,./images/grey-green/greygreen-3.jpg,./images/grey-green/greygreen-4.jpg,./images/grey-green/greygreen-5.jpg,./images/grey-green/greygreen-6.jpg,./images/grey-green/greygreen-7.jpg,./images/grey-green/greygreen-8.jpg,./images/grey-green/greygreen-9.jpg,./images/grey-green/grey-green-10.jpg,./images/grey-green/grey-green-11.jpg,./images/grey-green/grey-green-12.jpg,./images/grey-green/grey-green-13.jpg,./images/grey-green/grey-green-14.jpg,./images/grey-green/grey-green-15.jpg,./images/grey-green/grey-green-16.jpg,./images/grey-green/grey-green-17.jpg,./images/grey-green/grey-green-18.jpg,./images/grey-green/grey-green-19.jpg,./images/grey-green/grey-green-20.jpg,./images/grey-green/grey-green-21.jpg,./images/grey-green/grey-green-22.jpg,./images/grey-green/grey-green-23.jpg,./images/grey-green/grey-green-24.jpg';
+images['grey-grey'] = './images/grey-grey/grey-1.jpg,./images/grey-grey/grey-2.jpg,./images/grey-grey/grey-3.jpg,./images/grey-grey/grey-4.jpg,./images/grey-grey/grey-5.jpg,./images/grey-grey/grey-6.jpg,./images/grey-grey/grey-7.jpg,./images/grey-grey/grey-8.jpg,./images/grey-grey/grey-9.jpg,./images/grey-grey/grey-10.jpg,./images/grey-grey/grey-11.jpg,./images/grey-grey/grey-12.jpg,./images/grey-grey/grey-13.jpg,./images/grey-grey/grey-14.jpg,./images/grey-grey/grey-15.jpg,./images/grey-grey/grey-16.jpg,./images/grey-grey/grey-17.jpg,./images/grey-grey/grey-18.jpg,./images/grey-grey/grey-19.jpg,./images/grey-grey/grey-20.jpg,./images/grey-grey/grey-21.jpg,./images/grey-grey/grey-22.jpg,./images/grey-grey/grey-23.jpg,./images/grey-grey/grey-24.jpg';
+images['grey-red'] = './images/grey-red/greyred-1.jpg,./images/grey-red/greyred-2.jpg,./images/grey-red/greyred-3.jpg,./images/grey-red/greyred-4.jpg,./images/grey-red/greyred-5.jpg,./images/grey-red/greyred-6.jpg,./images/grey-red/greyred-7.jpg,./images/grey-red/greyred-8.jpg,./images/grey-red/greyred-9.jpg,./images/grey-red/grey-red-10.jpg,./images/grey-red/grey-red-11.jpg,./images/grey-red/grey-red-12.jpg,./images/grey-red/grey-red-13.jpg,./images/grey-red/grey-red-14.jpg,./images/grey-red/grey-red-15.jpg,./images/grey-red/grey-red-16.jpg,./images/grey-red/grey-red-17.jpg,./images/grey-red/grey-red-18.jpg,./images/grey-red/grey-red-19.jpg,./images/grey-red/grey-red-20.jpg,./images/grey-red/grey-red-21.jpg,./images/grey-red/grey-red-22.jpg,./images/grey-red/grey-red-23.jpg,./images/grey-red/grey-red-24.jpg';
+images['red-green'] = './images/red-green/redgreen-1.jpg,./images/red-green/redgreen-2.jpg,./images/red-green/redgreen-3.jpg,./images/red-green/redgreen-4.jpg,./images/red-green/redgreen-5.jpg,./images/red-green/redgreen-6.jpg,./images/red-green/redgreen-7.jpg,./images/red-green/redgreen-8.jpg,./images/red-green/redgreen-9.jpg,./images/red-green/red-green-10.jpg,./images/red-green/red-green-11.jpg,./images/red-green/red-green-12.jpg,./images/red-green/red-green-13.jpg,./images/red-green/red-green-14.jpg,./images/red-green/red-green-15.jpg,./images/red-green/red-green-16.jpg,./images/red-green/red-green-17.jpg,./images/red-green/red-green-18.jpg,./images/red-green/red-green-19.jpg,./images/red-green/red-green-20.jpg,./images/red-green/red-green-21.jpg,./images/red-green/red-green-22.jpg,./images/red-green/red-green-23.jpg,./images/red-green/red-green-24.jpg';
+images['red-grey'] = './images/red-grey/redgrey-1.jpg,./images/red-grey/redgrey-2.jpg,./images/red-grey/redgrey-3.jpg,./images/red-grey/redgrey-4.jpg,./images/red-grey/redgrey-5.jpg,./images/red-grey/redgrey-6.jpg,./images/red-grey/redgrey-7.jpg,./images/red-grey/redgrey-8.jpg,./images/red-grey/redgrey-9.jpg,./images/red-grey/red-grey-10.jpg,./images/red-grey/red-grey-11.jpg,./images/red-grey/red-grey-12.jpg,./images/red-grey/red-grey-13.jpg,./images/red-grey/red-grey-14.jpg,./images/red-grey/red-grey-15.jpg,./images/red-grey/red-grey-16.jpg,./images/red-grey/red-grey-17.jpg,./images/red-grey/red-grey-18.jpg,./images/red-grey/red-grey-19.jpg,./images/red-grey/red-grey-20.jpg,./images/red-grey/red-grey-21.jpg,./images/red-grey/red-grey-22.jpg,./images/red-grey/red-grey-23.jpg,./images/red-grey/red-grey-24.jpg';
+images['red-red'] = './images/red-red/red-1.jpg,./images/red-red/red-2.jpg,./images/red-red/red-3.jpg,./images/red-red/red-4.jpg,./images/red-red/red-5.jpg,./images/red-red/red-6.jpg,./images/red-red/red-7.jpg,./images/red-red/red-8.jpg,./images/red-red/red-9.jpg,./images/red-red/red-10.jpg,./images/red-red/red-11.jpg,./images/red-red/red-12.jpg,./images/red-red/red-13.jpg,./images/red-red/red-14.jpg,./images/red-red/red-15.jpg,./images/red-red/red-16.jpg,./images/red-red/red-17.jpg,./images/red-red/red-18.jpg,./images/red-red/red-19.jpg,./images/red-red/red-20.jpg,./images/red-red/red-21.jpg,./images/red-red/red-22.jpg,./images/red-red/red-23.jpg,./images/red-red/red-24.jpg';
+
+var site = {
+	init: function() {
+		site.accordion();
+		site.mainSlider();
+		site.thumbnailSlider();
+	},
+
+	mainSlider: function() {
+		jQuery('.main-slider').slick({
+			infinite: false,
+			asNavFor: '.thumbnail-slider',
+			dots: false,
+			arrows: false,
+			slidesToShow: 1,
+			slidesToScroll: 1
+		});
+
+		jQuery('.main-slider .slider-item').zoom();
+	},
+
+	thumbnailSlider: function() {
+		jQuery('.thumbnail-slider').slick({
+			infinite: false,
+			asNavFor: '.main-slider',
+			variableWidth: false,
+			centerMode: true,
+			centerPadding: '0',
+			speed: 0,
+			draggable: false,
+			dots: false,
+			arrows: false,
+			slidesToShow: 5,
+			slidesToScroll: 1
+		});
+	},
+
+	accordion: function() {
+		jQuery('.accordion-heading').click(function() {
+			var contentEl = jQuery(this).parents('.accordion-wrapper');
+
+			if (contentEl.hasClass('active')) {
+				contentEl.removeClass('active');
+				contentEl.find('.accordion-content').slideUp();
+			} else {
+				jQuery('.accordion-wrapper').removeClass('active');
+				jQuery('.accordion-content').slideUp();
+
+				contentEl.addClass('active');
+				contentEl.find('.accordion-content').slideDown();
+			}
+		});
+
+		jQuery('.accordion-wrapper .accordion-selection').click(function() {
+
+			var _this = jQuery(this);
+
+			if (_this.hasClass('active')) {
+				return false;
+			}
+
+			jQuery('body').append('<span class="loader" style="display: block;"></span>');
+
+			var targetEl = jQuery(this).data('target');
+			var imagesData = images[targetEl].split(',');
+			var sliderData = '';
+
+			if (imagesData.length > 0) {
+
+				for (var i = 0; i < imagesData.length; i++) {
+					sliderData += '<div class="slider-item"><img src="'+imagesData[i]+'" /></div>';
+				}
+
+				var csimage = new Image();
+
+				csimage.src = imagesData[0];
+
+				csimage.onload = function() {
+					jQuery('.main-slider').removeClass('slick-initialized slick-slider').html(sliderData);
+					jQuery('.thumbnail-slider').removeClass('slick-initialized slick-slider').html(sliderData);
+					site.mainSlider();
+					site.thumbnailSlider();
+
+					jQuery('.accordion-wrapper .accordion-selection').removeClass('active');
+					_this.addClass('active');
+					jQuery('body .loader').remove();
+				}
+			}
+
+			return false;
+		});
+	}
+}
+
+jQuery(document).ready(function() {
+	site.init();
+});
